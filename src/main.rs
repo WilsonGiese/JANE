5ff97fa9eedf97c32919eff7bc8c24800cff7302
WilsonGiese <giese.wilson@gmail.com>
2016-01-24 23:44:04 -0800

    Getting up and running; loading rom from disk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::Result;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    read_rom("roms/smb.nes").unwrap();
+}
+
+// Read bytes from NES rom from file
+// Common ROM format is iNES and NES 2.0
+// 		http://wiki.nesdev.com/w/index.php/INES
+fn read_rom<P: AsRef<Path>>(path: P) -> Result<Vec<u8>> {
+	let mut buffer = Vec::<u8>::new();
+
+	let mut rom = try!(File::open(path));
+	try!(rom.read_to_end(&mut buffer));
+
+	// Roms begin with first 3 bytes = "NES"
+	println!("First 3 bytes:");
+	for b in &buffer[0..3] {
+		println!("{:#x} {}", b, *b as char);
+	}
+
+	Ok(buffer)
 }
