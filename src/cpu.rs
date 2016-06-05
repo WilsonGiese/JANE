6b2f79d936d995dcbd99765e493b35cf331bd1bc
Wilson Giese <giese.wilson@gmail.com>
2016-06-05 14:08:54 -0700

    Refactor status register

--- src/cpu.rs
+++ src/cpu.rs
@@ -6,15 +6,18 @@ const RESET_VECTOR: u16 = 0xFFFC; // Location of first instruction in memory
 const IRQ_VECTOR: u16 = 0xFFFE;
 
 /// CPU Flags
-#[derive(Debug, Default)]
-pub struct Flags {
-	carry: bool,        // True if addition or shift carried, or subtraction didn't borrow
-	zero: bool, 	    // True if Last operation result is 0
-	irq_disable: bool,  // True if we want to inhibit IRQ interrupts (NMI allowed)
-	decimal: bool,      // True if decimal mode
-	overflow: bool,     // True if last ADC or SBC resulted in signed overflow
-	negative: bool      // True if set bit 7 of last operation
-}
+const CARRY_FLAG:    u8 = 1;      // Set if addition or shift carried, or subtraction didn't borrow
+const ZERO_FLAG:     u8 = 1 << 1; // Set if Last operation result is 0
+const IRQ_FLAG:      u8 = 1 << 2; // Set if we want to inhibit IRQ interrupts (NMI allowed)
+const DECIMAL_FLAG:  u8 = 1 << 3; // Set if decimal mode
+const OVERFLOW_FLAG: u8 = 1 << 6; // Set if last ADC or SBC resulted in signed overflow
+const NEGATIVE_FLAG: u8 = 1 << 7; // Set if set bit 7 of last operation
+
+// Set if BRK occured. The docs on nesdev.com are a bit confusing for this, says this bit has no
+// effect, but also says "The only way for an IRQ handler to distinguish IRQ from BRK is to read the
+// flags byte from the stack and test bit 4"
+// We'll keep this flag for now and set it in the BRK instruction before pushing to the stack
+const BREAK_FLAG:    u8 = 1 << 4;
 
 /// CPU Registers
 #[derive(Debug, Default)]
@@ -24,7 +27,7 @@ struct Registers {
 	y: u8,    // ''
 	s: u8,    // Stack pointer
 	pc: u16,  // Program counter (2 bytes wide)
-	status: Flags, // Status register used by various instructions and the ALU
+	status: u8 // Status register used by various instructions and the ALU
 }
 
 /// Model for the 6502 Microprocessor
@@ -37,7 +40,6 @@ pub struct CPU {
 impl CPU {
 
 	pub fn new(cartridge: Box<Memory>) -> CPU {
-
 		CPU {
 			registers: Registers::default(),
 			ram: ReadWriteMemory::new(0x800),
@@ -52,8 +54,8 @@ impl CPU {
 		self.registers.x = 0;
 		self.registers.y = 0;
 		self.registers.s = 0xFD;
-		self.registers.status.irq_disable = true;
 		self.registers.pc = self.cartridge.loadw(RESET_VECTOR);
+		self.set_status(IRQ_FLAG, true);
 	}
 
 	pub fn power_up_with_pc_override(&mut self, pc: u16) {
@@ -61,8 +63,8 @@ impl CPU {
 		self.registers.x = 0;
 		self.registers.y = 0;
 		self.registers.s = 0xFD;
-		self.registers.status.irq_disable = true;
 		self.registers.pc = pc;
+		self.set_status(IRQ_FLAG, true);
 	}
 
 	/// Emulate CPU reset
@@ -79,6 +81,20 @@ impl CPU {
 			let instruction = self.load_pc();
 			self.execute(instruction);
 		}
+	}
+
+	// Status register operations
+
+	fn set_status(&mut self, flag: u8, value: bool) {
+		if value {
+			self.registers.status |= flag;
+		} else {
+			self.registers.status &= !flag;
+		}
+	}
+
+	fn get_status(&self, flag: u8) -> bool {
+		self.registers.status & flag != 0
 	}
 
 
@@ -180,8 +196,8 @@ impl CPU {
 	}
 
 	fn set_zn(&mut self, value: u8) {
-		self.registers.status.zero = value == 0;
-		self.registers.status.negative = value & 0x80 != 0;
+		self.set_status(ZERO_FLAG, value == 0);
+		self.set_status(NEGATIVE_FLAG, value & 0x80 != 0);
 	}
 
 	pub fn execute(&mut self, instruction: u8) {
@@ -290,17 +306,14 @@ impl CPU {
 		let value = self.load(address);
 		let mut new_a = self.registers.a as u16 + value as u16;
 
-		if self.registers.status.carry {
+		if self.get_status(CARRY_FLAG) {
 			new_a += 1;
 		}
-
-		if new_a > 0xFF {
-			self.registers.status.carry = true;
-		}
+		self.set_status(CARRY_FLAG, new_a > 0xFF);
 
 		if (self.registers.a ^ value) & 0x80 == 0 {
 			if (self.registers.a ^ new_a as u8) & 0x80 == 0x80 {
-				self.registers.status.overflow = true;
+				self.set_status(OVERFLOW_FLAG, true);
 			}
 		}
 		self.set_zn(new_a as u8);
@@ -320,7 +333,7 @@ impl CPU {
 	// M << 1 -> M
 	fn asl(&mut self, address: u16) {
 		let mut value = self.load(address);
-		self.registers.status.carry = value & 0x80 == 0x80;
+		self.set_status(CARRY_FLAG, value & 0x80 == 0x80);
 		value <<= 1;
 		self.set_zn(value);
 		self.store(address, value);
@@ -329,7 +342,8 @@ impl CPU {
 	// ASL - Shift accumulator left one bit
 	// A << 1 -> A
 	fn asla(&mut self) {
-		self.registers.status.carry = self.registers.a & 0x80 == 0x80;
+		let a = self.registers.a;
+		self.set_status(CARRY_FLAG, a & 0x80 == 0x80);
 		let new_a = self.registers.a << 1;
 		self.set_zn(new_a);
 		self.registers.a = new_a;
@@ -339,7 +353,7 @@ impl CPU {
 	// Branch on Carry == 0
 	// Uses relative addressing mode; PC + value @ address
 	fn bcc(&mut self) {
-		if !self.registers.status.carry {
+		if !self.get_status(CARRY_FLAG) {
 			self.registers.pc = self.relative_mode();
 		}
 	}
@@ -347,7 +361,7 @@ impl CPU {
 	// BCS - Branch on carry set
 	// Branch on Carry == 1
 	fn bcs(&mut self) {
-		if self.registers.status.carry {
+		if self.get_status(CARRY_FLAG) {
 			self.registers.pc = self.relative_mode();
 		}
 	}
@@ -355,7 +369,7 @@ impl CPU {
 	// BEQ - Branch on Zero
 	// Branch on Zero == 1
 	fn beq(&mut self) {
-		if self.registers.status.zero {
+		if self.get_status(ZERO_FLAG) {
 			self.registers.pc = self.relative_mode();
 		}
 	}
@@ -364,33 +378,32 @@ impl CPU {
 	// A & M, M7 -> N, M6 -> V
 	fn bit(&mut self, address: u16) {
 		let value = self.load(address);
-		self.registers.status.negative = value & 0x80 == 0x80;
-		self.registers.status.overflow = value & 0x40 == 0x40;
-		self.registers.status.zero = self.registers.a & value == 0;
+		self.set_status(NEGATIVE_FLAG, value & 0x80 == 0x80);
+		self.set_status(OVERFLOW_FLAG, value & 0x40 == 0x40);
+		let a = self.registers.a;
+		self.set_status(ZERO_FLAG, a & value == 0);
 	}
 
 	// BMI - Branch on result minus
 	// Branch on Negative == 1
 	fn bmi(&mut self) {
-		if self.registers.status.negative {
+		if self.get_status(NEGATIVE_FLAG) {
 			self.registers.pc = self.relative_mode();
 		}
 	}
 
-
-
 	// BNE - Branch on result not zero
 	// Branch on Zero == 0
 	fn bne(&mut self) {
-		if !self.registers.status.zero {
+		if !self.get_status(ZERO_FLAG)  {
 			self.registers.pc = self.relative_mode();
 		}
 	}
 
 	// BPL - Branch on result plus
-	// Branch on Negative  == 0
+	// Branch on Negative == 0
 	fn bpl(&mut self) {
-		if self.registers.status.negative {
+		if !self.get_status(NEGATIVE_FLAG)  {
 			self.registers.pc = self.relative_mode();
 		}
 	}
@@ -407,7 +420,7 @@ impl CPU {
 	// BVC - Branch on overflow clear
 	// Branch on Overflow == 0
 	fn bvc(&mut self) {
-		if !self.registers.status.overflow {
+		if !self.get_status(OVERFLOW_FLAG)  {
 			self.registers.pc = self.relative_mode();
 		}
 	}
@@ -415,7 +428,7 @@ impl CPU {
 	// BVS - Branch on overflow set
 	// Branch on Overflow == 1
 	fn bvs(&mut self) {
-		if self.registers.status.overflow {
+		if self.get_status(OVERFLOW_FLAG)  {
 			self.registers.pc = self.relative_mode();
 		}
 	}
@@ -499,27 +512,27 @@ impl CPU {
 
 	/// SEI - Set interrupt disable status
 	fn sei(&mut self) {
-		self.registers.status.irq_disable = true;
+		self.set_status(IRQ_FLAG, true);
 	}
 
 	/// CLD - Clear decimal status
 	fn cld(&mut self) {
-		self.registers.status.decimal = false;
+		self.set_status(DECIMAL_FLAG, false);
 	}
 
 	/// CLC - Clear carry flag
 	fn clc(&mut self) {
-		self.registers.status.carry = false;
+		self.set_status(CARRY_FLAG, false);
 	}
 
 	/// CLI - Clear interrupt disable flag
 	fn cli(&mut self) {
-		self.registers.status.irq_disable = false;
+		self.set_status(IRQ_FLAG, false);
 	}
 
 	/// CLV - Clear overflow flag
 	fn clv(&mut self) {
-		self.registers.status.overflow = false;
+		self.set_status(OVERFLOW_FLAG, false);
 	}
 
 	fn nop(&self) { }
